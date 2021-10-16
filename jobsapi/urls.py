b234378b72cc1dd1234e61bf20e5e43fcb92d4f8
Madaraka02 <victormadaraka02@gmail.com>
2021-10-16 10:04:30 +0300

    ...

--- jobsapi/urls.py
+++ jobsapi/urls.py
@@ -3,5 +3,5 @@ from .views import Joblist, JobDetailView
 
 urlpatterns = [
     path('', Joblist.as_view()),
-    path('<int:id>/', JobDetailView.as_view()),
+    path('<int:id>', JobDetailView.as_view()),
 ]
