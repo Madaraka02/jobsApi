360bbe71372319b2d83266aea4125c34cee43825
Madaraka02 <victormadaraka02@gmail.com>
2021-10-16 10:09:44 +0300

    ...

--- company/urls.py
+++ company/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/jobs/', include('jobsapi.urls')),
     path('api/auth/', include('authentication.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
